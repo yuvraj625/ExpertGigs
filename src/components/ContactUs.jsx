cc0d7f079650be3e28e05177dcc5e92e85d5ad06
Yuvraj_Singh310 <109100137+yuvraj625@users.noreply.github.com>
2023-05-26 14:47:27 +0530

    Initial commit

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div id="contactus">
+      <h1>Contact Us</h1>
+      <p>
+        Thank you for your interest in ExpertGigs. We'd love to hear from you!
+      </p>
+      <p>
+        Let’s make something new, different and more meaningful or make thing
+        more visual or conceptual? Just Say Hello!
+      </p>
+      <ul>
+        <li>Email: yuvrajsinghtoriya@gmail.com</li>
+        <li>Phone: +91 7579217338</li>
+        <li>Address: DehraDun,Uttarakhand India</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactUs;
